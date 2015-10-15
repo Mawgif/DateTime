06a287478403335fd65eea37c944dafba5192f26
abdulbasith <basee0304@gmail.com>
2015-10-15 22:40:52 +0300

    basith commit

--- Datetime/src/com/example/datetime/TimeChanged.java
+++ Datetime/src/com/example/datetime/TimeChanged.java
@@ -26,7 +26,7 @@ public class TimeChanged extends BroadcastReceiver {
         // you can check the updated time using Calendar c = Calendar.getInstance();
     	
     	System.out.println("Date Changed !");
-    	
+    	System.out.println("Basith !");
     	
     /*	try {
 			Settings.System.getInt(context.getContentResolver(), Settings.System.TIME_12_24);
